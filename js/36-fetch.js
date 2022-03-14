3d4e02c92fad76bd8293a9aa660003bb45f1e961
Cristian Primero <soyskiper@gmail.com>
2022-03-14 14:13:00 -0600

    Mod: datos-externos.html js/36

--- js/36-fetch.js
+++ js/36-fetch.js
@@ -1,35 +1,45 @@
 'use strict'
 
-
 var div_usuarios = document.querySelector("#usuarios");
+var div_janet = document.querySelector("#janet");
 
-//Fetch (ajax) y peticiones a servicios /apis rest
-var usuarios = [];
 
-//Páginas de prueba para apis
-//https://jsonplaceholder.typicode.com/users
-fetch('https://reqres.in/api/users?page=1')
-    //Tomamos los datos
-    //Posteriormente lo transformamos a un objeto con el método .json para que pueda ser leído por javascript
+    getUsuarios()
+        .then(data => data.json())
+        .then(users => {
+        listadoUsuarios(users.data);
+        return getJanet();
+    })
     .then(data => data.json())
-        /* Otra manera de hacer el call back
-            .then(function(data){
-                return data.json()
-            })
-        */
-    .then(users => {
-        //Dentro de usuario guardamos la data
-        usuarios = users.data;
-        console.log(usuarios);
+    .then(user => {
+        mostrarJanet(user.data);
 
+    });
 
+    function getUsuarios(){
+        return fetch('https://reqres.in/api/users');
+    }
 
+    function getJanet(){
+        return fetch('https://reqres.in/api/users/2');
+    }
+
+    function listadoUsuarios(usuarios){
         usuarios.map((user, i)=>{
             let nombre = document.createElement("h3");
-
             nombre.innerHTML = i + ".  "+user.first_name +" " + user.last_name;
-
             div_usuarios.appendChild(nombre);
             document.querySelector(".loading").style.display = 'none';
         });
-    });
+    }
+
+    function mostrarJanet(user){
+        let nombre = document.createElement("h4");
+        let avatar = document.createElement("img");
+        nombre.innerHTML = user.first_name       + " " + user.last_name;
+        avatar.src = user.avatar;
+        avatar.width = '100';
+        div_janet.appendChild(nombre);
+        div_janet.appendChild(avatar);
+        document.querySelector("#janet .loading").style.display = 'none';
+    }
